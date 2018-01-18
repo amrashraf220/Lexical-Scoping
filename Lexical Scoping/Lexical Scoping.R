875321fe7195592fa12ff973af863c86e52b9a5f
amrashraf220 <35413586+amrashraf220@users.noreply.github.com>
2018-01-18 20:50:19 +0200

    Add files via upload

---
+++ Lexical Scoping/Lexical Scoping.R
@@ -0,0 +1,24 @@
+makeCacheMatrix <- function(x = matrix()) {
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(i) inverse <- i
+	getinverse <- function() inverse
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+
+cacheSolve <- function(x, ...) {
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+        	message("getting cached inverse")
+        	return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
+}
